977be715397efee2275defbfd95f259598b2c83c
Imam Nc <contact.imamnc@gmail.com>
2025-01-16 09:52:21 +0700

    Adding supervisor functionality

--- core/php80/docker-entrypoint.sh
+++ core/php80/docker-entrypoint.sh
@@ -9,5 +9,13 @@ else
     echo "Cron file not found. Skipping crontab setup."
 fi
 
-# Run the default command (e.g., php-fpm)
-exec "$@"
+# Check if there are any .ini files in /etc/supervisor.d/
+if compgen -G "/etc/supervisor.d/*.ini" > /dev/null; then
+    echo "Supervisor .ini files found. Starting supervisord..."
+    supervisord -c /etc/supervisord.conf
+else
+    echo "No Supervisor .ini files found in /etc/supervisor.d/. Skipping supervisord setup."
+fi
+
+# Prevent container exit after execution finish
+tail -f /dev/null
